b51426abe7a3915a7b4120679786f8d28ae6af4d
trian damai <triannurizkillah@gmail.com>
2021-10-09 11:01:55 +0700

    feat: init

---
+++ Learn Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn Swift
+//
+//  Created by macbook on 09/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn Swift/Learn_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftApp.swift
+//  Learn Swift
+//
+//  Created by macbook on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
